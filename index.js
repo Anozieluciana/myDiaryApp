dea23de588d6d85756e07e5451db63acb61ac244
Anozieluciana <anoziechiderasilverlin@gmail.com>
2022-06-10 15:58:05 +0100

    hgjk

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ require("./utils/db");
 const port = process.env.PORT || 2020
 const app = express();
 
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.status(200).json({ message: "This is best place to be" });
-});
+// app.get("/", (req, res) => {
+// 	res.status(200).json({ message: "This is best place to be" });
+// });
 
 app.use("/api/user", require("./router/userRouter"));
 app.use("/api/diary", require("./router/diaryRouter"));
